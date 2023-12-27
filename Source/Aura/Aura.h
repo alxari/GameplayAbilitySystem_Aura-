7bda57c03085f1369c78cd880f559598a25b3cbb
Alexander <martin.alxri@gmail.com>
2023-12-27 01:48:14 -0800

    End of Section2

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -1,6 +1,8 @@
 // Copyright Druid Mechanics
 
 #pragma once
+#pragma warning(disable: 4800)
 
 #include "CoreMinimal.h"
 
+#define CUSTOM_DEPTH_RED 250
--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -1,15 +1,26 @@
 // Copyright Druid Mechanics
 
+#pragma warning(disable:4800)
 
 #include "Character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetRenderCustomDepth(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
